7f1ae3535beee4de58871d6240a2d595ae640524
Kevin Vance <kvance@kvance.com>
2015-04-11 21:24:30 -0400

    Implemented in-animation

--- animation/animate.py
+++ animation/animate.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
 
+import signal
 import sys
 
-from PyQt4 import QtGui
+from PyQt4 import QtCore
 from PyQt4.QtCore import Qt
+from PyQt4 import QtGui
 
 def main():
+    # Use the default ctrl-c handler so we can be killed while running
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
     app = QtGui.QApplication(sys.argv)
 
     # Load images
     bg_pixmap = QtGui.QPixmap("background.png")
-    segments = [QtGui.QPixmap("segment{}.png".format(i)) for i in range (0,5)]
+    segment_pixmaps = [QtGui.QPixmap("segment{}.png".format(i)) for i in range (0,5)]
 
     # Place in scene
     scene = QtGui.QGraphicsScene()
-    scene.addPixmap(bg_pixmap)
-    for segment in segments:
-        scene.addPixmap(segment)
+    background = scene.addPixmap(bg_pixmap)
+    segments = [scene.addPixmap(pixmap) for pixmap in segment_pixmaps]
+    print("segment transform origin =", segments[0].transformOriginPoint().x(), segments[0].transformOriginPoint().y())
 
     # Create and show scene window
     view = QtGui.QGraphicsView(scene)
@@ -30,8 +35,34 @@ def main():
     view.setFrameStyle(QtGui.QFrame.NoFrame)
     view.show()
 
-    # Quit on key press
-    view.keyPressEvent = lambda event: view.close()
+    animations = {}
+
+    def animate():
+        timeline = QtCore.QTimeLine(400)
+        timeline.setUpdateInterval(16)
+        timeline.setEasingCurve(QtCore.QEasingCurve.OutQuad)
+
+        scale = QtGui.QGraphicsItemAnimation()
+        scale.setItem(background)
+        scale.setTimeLine(timeline)
+        scale.setScaleAt(0.0, 0.0, 0.0)
+        scale.setScaleAt(1.0, 1.0, 1.0)
+        animations['scale'] = scale  # Prevent GC
+
+        STEP = 0.2
+        for i, segment in enumerate(reversed(segments)):
+            rotate = QtGui.QGraphicsItemAnimation()
+            rotate.setItem(segment)
+            rotate.setTimeLine(timeline)
+            rotate.setRotationAt(0.0, -90.0)
+            rotate.setRotationAt(0.0 + STEP * i, -90.0)
+            rotate.setRotationAt(1.0, 0.0)
+            animations['rotate{}'.format(i)] = rotate  # Prevent GC
+
+        timeline.start()
+
+    view.mousePressEvent = lambda e: animate()
+
 
     sys.exit(app.exec_())
 
