dacdfbb565ac2ac26d311820a157bddf178fe01e
Kevin Vance <kvance@kvance.com>
2015-04-15 21:56:00 -0400

    Add example transforms for the other corners

--- animation/animate.py
+++ animation/animate.py
@@ -7,6 +7,21 @@ from PyQt4 import QtCore
 from PyQt4.QtCore import Qt
 from PyQt4 import QtGui
 
+
+class SegmentItem(QtGui.QGraphicsItem):
+    def __init__(self, filename):
+        super().__init__()
+        self.pixmap = QtGui.QPixmap(filename)
+        self.value = 1.0
+
+    def paint(self, painter, option, widget):
+        painter.drawPixmap(0, 0, self.pixmap)
+        # TODO: fill with value (0-1)
+
+    def boundingRect(self):
+        return QtCore.QRectF(0, 0, self.pixmap.width(), self.pixmap.height())
+
+
 def main():
     # Use the default ctrl-c handler so we can be killed while running
     signal.signal(signal.SIGINT, signal.SIG_DFL)
@@ -15,13 +30,13 @@ def main():
 
     # Load images
     bg_pixmap = QtGui.QPixmap("background.png")
-    segment_pixmaps = [QtGui.QPixmap("segment{}.png".format(i)) for i in range (0,5)]
+    segments = [SegmentItem("segment{}.png".format(i)) for i in range(0, 5)]
 
     # Place in scene
     scene = QtGui.QGraphicsScene()
     background = scene.addPixmap(bg_pixmap)
-    segments = [scene.addPixmap(pixmap) for pixmap in segment_pixmaps]
-    print("segment transform origin =", segments[0].transformOriginPoint().x(), segments[0].transformOriginPoint().y())
+    for segment in segments:
+        scene.addItem(segment)
 
     # Create and show scene window
     view = QtGui.QGraphicsView(scene)
@@ -34,6 +49,19 @@ def main():
     view.setAttribute(Qt.WA_TranslucentBackground)
     view.setFrameStyle(QtGui.QFrame.NoFrame)
     view.show()
+
+    # Use negative scale (and sometimes transform) to flip
+    #
+    # top-right
+    # view.scale(-1.0, 1.0)
+    #
+    # bottom-left
+    # view.scale(1.0, -1.0)
+    # view.translate(0, -200)
+    #
+    # bottom-right
+    # view.scale(-1.0, -1.0)
+    # view.translate(0, -200)
 
     animations = {}
 
